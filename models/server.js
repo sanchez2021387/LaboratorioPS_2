f8650ee5699ce7740b3d8b5b4ce9f47882a252f3
jsanchez <jsanchez2021387@gmail.com>
2024-02-16 07:41:50 -0600

    Finalizado la conexion de datos y la base de datos

---
+++ models/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+        
+        this.connectarDB();
+        this.middlewares();
+    }
+
+    async connectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    listen(){
+        this.app.listen(this.port, () =>{
+            console.log('Servidor ejecutado y escuchado');
+        }) 
+    }
+}
+module.exports = Server;
